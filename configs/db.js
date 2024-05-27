f5fb3e7f5aefcd4536fa580cb50e274896daaa3b
Rajpateriya <pateriyaraj52@gmail.com>
2024-05-27 21:11:47 +0530

    backend

---
+++ configs/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI)   
+        
+        console.log(`MongoDB connected..`)
+    } catch (error) {
+       console.log(`Error : connectnig with mongoDB`);
+       process.exit(1); 
+    }
+}
+
+module.exports = connectDB;
